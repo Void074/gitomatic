49a561d13126d4e8041eb513e6cad41e7b7529d7
Gedare <ggedare074@protonmail.com>
2023-02-21 11:20:39 +1000

    initial commit

---
+++ push.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+pid=$(pidof ssh-agent)
+
+if [[ ! -z  $pid ]]; then
+  # else run this block of code
+  echo "stopping ssh-agent service with pidof: $(pidof ssh-agent)"
+  kill -9 $(pidof ssh-agent)
+  sleep 2
+  echo "starting ssh-agent"
+  eval `ssh-agent`
+  sleep 2
+  echo "setting up ssh-key"
+  ssh-add ~/.ssh/gdorke@wls2debian
+  sleep 2
+  git commit $1
+fi
