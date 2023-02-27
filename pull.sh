973123c59cdd047d3f0c3b00f10d9e72cbcae33f
Gedare <ggedare074@protonmail.com>
2023-02-27 17:07:02 +1000

    feat(pull.sh) adding support for git pull

---
+++ pull.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+PID_OF_SSH_AGENT=$(pidof ssh-agent)
+REMOTE_URL=$(git remote -v)
+PATH_TO_SSH_KEY="path/to/ssh/key/here"
+
+if [[ -z "$REMOTE_URL" ]]; then
+   echo "unable to pull, URL to repository not set"
+  exit 1
+else
+  echo "pushing commit to remote repository"
+  if [[ ! -z  $PID_OF_SSH_AGENT ]]; then
+    echo "stopping ssh-agent service with pidof: $PID_OF_SSH_AGENT"
+    kill -9 $PID_OF_SSH_AGENT
+    sleep 2
+    echo "starting ssh-agent"
+    eval `ssh-agent`
+    sleep 2
+    echo "setting up ssh-key"
+    ssh-add $PATH_TO_SSH_KEY
+    sleep 2
+    git pull origin main
+  fi
+fi
