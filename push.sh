9ffff59fba614673fb11b466cb8e14d3de9a8a3b
Gedare <ggedare074@protonmail.com>
2023-02-27 14:00:21 +1000

    refactor(push.sh) added global variables for easy modification

--- push.sh
+++ push.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
   
-pid=$(pidof ssh-agent)
-remote_url=$(git remote -v)
+PID_OF_SSH_AGENT=$(pidof ssh-agent)
+REMOTE_URL=$(git remote -v)
+PATH_TO_SSH="Enter/path/to/ssh/key/here"
 
-if [[ -z $remote_url ]]; then
+if [[ -z "$REMOTE_URL" ]]; then
   echo "unable to push changes! URL to repository not set"
   exit 1
 else
-  echo "pushing commit to remote repositry"
-   
-  if [[ ! -z  $pid ]]; then
-    # else run this block of code
-    echo "stopping ssh-agent service with pidof: $(pidof ssh-agent)"
-    kill -9 $(pidof ssh-agent)
+  echo "pushing commit to remote repository"
+  if [[ ! -z  $PID_OF_SSH_AGENT ]]; then
+    echo "stopping ssh-agent service with pidof: $PID_OF_SSH_AGENT"
+    kill -9 "$PID_OF_SSH_AGENT"
     sleep 2
     echo "starting ssh-agent"
     eval `ssh-agent`
     sleep 2
     echo "setting up ssh-key"
-    ssh-add ~/path/to/ssh/kay
+    ssh-add "$PATH_TO_SSH_KEY"
     sleep 2
     git commit $1
   fi
